927071d1da6b932412372d4390b55f5a7af1ad51
kevinanielsen <keveran@gmail.com>
2023-11-23 22:03:35 +0100

    style: fix centering in contact me section

--- app/components/sections/contact-me.tsx
+++ app/components/sections/contact-me.tsx
@@ -54,7 +54,7 @@ const ContactMe = () => {
   };
 
   return (
-    <section id="contact" className="p-8 flex flex-col gap-4items-center">
+    <section id="contact" className="p-8 flex flex-col gap-4 items-center">
       <div className="max-w-7xl gap-4 flex flex-col p-8 w-full">
         <h2 className="text-4xl font-bold text-center">Contact Me</h2>
         <form className="flex flex-col sm:flex-row gap-4" ref={formRef}>
