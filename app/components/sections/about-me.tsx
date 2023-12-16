c5425751e75b4cf9d0d1b17358515fe399fe55a3
kevinanielsen <keveran@gmail.com>
2023-12-16 20:59:08 +0100

    chore: change ' to &apos;

--- app/components/sections/about-me.tsx
+++ app/components/sections/about-me.tsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
       <div className="max-w-7xl gap-4 flex flex-col p-8">
         <h2 className="text-4xl font-bold">About Me</h2>
         <p>
-          Hello, I'm Kevin - a self-taught front-end web developer with a
+          Hello, I&apos;m Kevin - a self-taught front-end web developer with a
           passion for building beautiful, user-friendly websites and web
           applications. I specialize in using technologies like React.js,
           TailwindCSS, TypeScript, and Next.js to create intuitive interfaces
@@ -26,22 +26,22 @@ const AboutMe = () => {
           from multiple angles to find the best possible solution. My typical
           workflow involves brainstorming features, designing layouts with
           Figma, setting up the backend, coding components, and writing tests
-          along the way. I'm always looking for new technologies and frameworks
-          to explore, and I'm open to working with clients from any industry or
-          background.
+          along the way. I&apos;m always looking for new technologies and
+          frameworks to explore, and I&apos;m open to working with clients from
+          any industry or background.
           <br />
           <br />
-          Although I'm still relatively new to the field of front-end
-          development, I'm proud of the progress I've made so far. I've
-          completed courses on test-driven development, and building web apps
-          with React.js, HTML, and Typescript. I've also developed a workout
-          tracker with features such as tracking your progression on a chosen
-          exercise.
+          Although I&apos;m still relatively new to the field of front-end
+          development, I&apos;m proud of the progress I&apos;ve made so far.
+          I&apos;ve completed courses on test-driven development, and building
+          web apps with React.js, HTML, and Typescript. I&apos;ve also developed
+          a workout tracker with features such as tracking your progression on a
+          chosen exercise.
           <br />
           <br />
-          If you're looking for a front-end web developer who's passionate about
-          creating beautiful, functional, and user-friendly websites and web
-          applications, I'd love to hear from you!
+          If you&apos;re looking for a front-end web developer who&apos;s
+          passionate about creating beautiful, functional, and user-friendly
+          websites and web applications, I&apos;d love to hear from you!
         </p>
         <div className="flex gap-4">
           <motion.button
