2a5433ec917ab37a9f7a664d10d4cd8c4b117ddc
Kevin Nielsen <kevinanielsen@outlook.com>
2024-04-30 14:26:59 +0200

    remove unused package

--- app/components/sections/experience.tsx
+++ app/components/sections/experience.tsx
@@ -9,10 +9,13 @@ const Experience: React.FC = () => {
     >
       <div className="max-w-7xl gap-4 flex flex-col p-8 w-full">
         <h2 className="text-4xl font-bold text-center">Experience</h2>
-        <div>
-          <h3 className="text-2xl">Software Developer Intern</h3>
-          <h4 className="text-xl font-bold">Fysiofresh</h4>
-          <h5>Jan. 2024 - Today</h5>
+        <div className="flex gap-4">
+
+          <div className="">
+            <h3 className="text-2xl">Software Developer Intern</h3>
+            <h4 className="text-xl font-bold">Fysiofresh</h4>
+            <h5>Jan. 2024 - Today</h5>
+          </div>
         </div>
       </div>
     </section>
