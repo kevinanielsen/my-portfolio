39fcdb7374c48bdf0ff245b1dad613947fa4407e
Kevin Nielsen <kevinanielsen@outlook.com>
2024-04-30 15:11:04 +0200

    start work on experience (#17)
    * start work on experience
    
    * remove unused package

---
+++ app/components/sections/experience.tsx
@@ -0,0 +1,25 @@
+const Experience: React.FC = () => {
+  return (
+    <section
+      id="about"
+      className="
+        dark:bg-dark
+        flex flex-col items-center
+      "
+    >
+      <div className="max-w-7xl gap-4 flex flex-col p-8 w-full">
+        <h2 className="text-4xl font-bold text-center">Experience</h2>
+        <div className="flex gap-4">
+
+          <div className="">
+            <h3 className="text-2xl">Software Developer Intern</h3>
+            <h4 className="text-xl font-bold">Fysiofresh</h4>
+            <h5>Jan. 2024 - Today</h5>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Experience
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import AboutMe from './components/sections/about-me'
 import TechStack from './components/sections/tech-stack'
 import Projects from './components/sections/projects'
 import ContactMe from './components/sections/contact-me'
+import Experience from './components/sections/experience'
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <main>
         {/* Title */}
         <Title />
+        {/* Experience */}
+        <Experience />
         {/* About Me */}
         <AboutMe />
         {/* Tech Stack */}
