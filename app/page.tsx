38d760208be91b70a7fda7cbfe975f7827cb90d0
Kevin Nielsen <kevinanielsen@outlook.com>
2024-04-30 14:22:26 +0200

    start work on experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Navbar from "./components/Navbar";
-import Title from "./components/sections/title";
-import AboutMe from "./components/sections/about-me";
-import TechStack from "./components/sections/tech-stack";
-import Projects from "./components/sections/projects";
-import ContactMe from "./components/sections/contact-me";
+import Navbar from './components/Navbar'
+import Title from './components/sections/title'
+import AboutMe from './components/sections/about-me'
+import TechStack from './components/sections/tech-stack'
+import Projects from './components/sections/projects'
+import ContactMe from './components/sections/contact-me'
+import Experience from './components/sections/experience'
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <main>
         {/* Title */}
         <Title />
+        {/* Experience */}
+        <Experience />
         {/* About Me */}
         <AboutMe />
         {/* Tech Stack */}
